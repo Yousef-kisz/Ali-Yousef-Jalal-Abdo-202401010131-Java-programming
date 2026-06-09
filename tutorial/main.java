fce68e8c36901c186b14d9da3a130295eeb203b6
Yousef-kisz <tedu1312@gmail.com>
2026-06-09 06:50:01 +0000

    implement student class and main program

---
+++ tutorial/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("yousef", 23, 3.5);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
